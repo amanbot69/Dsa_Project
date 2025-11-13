3eb44781a9e0fbb01e74eae6f71e7fe832986f2b
amanbot69 <b24cs1006@iitj.ac.in>
2025-11-13 21:38:31 +0530

    trip feature added

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+//All Header File of the code 
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -9,12 +10,44 @@
 #include <sstream>
 #include <algorithm>
 #include <set>
+#include <numeric>  
+#include <climits>  
+
 using namespace std;
 
+//Structure declaration
 struct Edge {
     string to;
     int val;
 };
+
+struct FullPath {
+    vector<string> route;
+    int totalValue;
+};
+
+//Declarations of all the function of the code
+void printHeader();
+string toLowerManual(string s);
+bool cityExists(const string &city);
+void showAllCities();
+void loadGraph(map<string, vector<Edge>>& graph, string mode);
+void Alltypeofpath();
+void costpath();
+void timepath();
+void overallbest();
+void findTopKPaths(map<string, vector<Edge>> graph, string start, string end, int K);
+void dfsAllPaths(string current, string dest, map<string, vector<Edge>>& graph,map<string, bool>& visited, vector<string>& path, int cost, vector<FullPath>& allPaths);
+vector<FullPath> removeDuplicatePaths(const vector<FullPath>& allPaths);
+void changeinthepath();
+void newcity();
+void newpath();
+void deletepath();
+void updatePath();
+void planMyTrip(map<string, vector<pair<string, pair<int, int>>>> &graph, map<string, bool> &cityExist);
+
+
+//Data loading function and checking function(Heapler Functions)
 
 string toLowerManual(string s) {
     for (char &ch : s)
@@ -65,75 +98,100 @@ void loadGraph(map<string, vector<Edge>>& graph, string mode) {
     file.close();
 }
 
-struct FullPath {
-    vector<string> route;
-    int totalValue;
-};
-
-
-void dfsAllPaths(string current, string dest, map<string, vector<Edge>>& graph,
-                 map<string, bool>& visited, vector<string>& path, int cost,
-                 vector<FullPath>& allPaths) {
-    if (current == dest) {
-        allPaths.push_back({path, cost});
-        return;
-    }
-
-    visited[current] = true;
-    for (auto& e : graph[current]) {
-        if (!visited[e.to]) {
-            path.push_back(e.to);
-            dfsAllPaths(e.to, dest, graph, visited, path, cost + e.val, allPaths);
-            path.pop_back(); 
-        }
-    }
-    visited[current] = false;
+bool cityExists(const string &city) {
+    ifstream file("city.txt");
+    if (!file.is_open()) return false;
+    string name;
+    while (file >> name) if (name == city) return true;
+    return false;
 }
 
-vector<FullPath> removeDuplicatePaths(const vector<FullPath>& allPaths) {
-    vector<FullPath> uniquePaths;
-    set<string> seen;
-
-    for (const auto& p : allPaths) {
-        string pathString;
-        for (const auto& city : p.route)
-            pathString += city + "->";
-        pathString.pop_back(); 
-
-        if (!seen.count(pathString)) {
-            seen.insert(pathString);
-            uniquePaths.push_back(p);
+//Main Function of the code
+int main() {
+    printHeader();
+    Sleep(2000);
+    system("cls");
+    while (true) {
+        cout << "\033[1;35m--- MAIN MENU ---\033[0m\n";
+        cout << "1. Find a path\n";
+        cout << "2. Modify paths/cities\n";
+        cout << "3. Plan for a trip\n";
+        cout << "4. Exit\n";
+        cout << "Enter your choice: ";
+        string input;
+        cin >> input;
+        if(input.size()>=2 || (input[0]<'0' && input[0]>'9')){
+            cout<<"Invalid Input"<<endl ;
+            Sleep(1000);
+            continue ;
         }
-    }
+        int choice = input[0] - '0';
+        system("cls");
+        switch (choice) {
+            case 1: Alltypeofpath(); break;
+            case 2: changeinthepath(); break;
+            case 3: {
+            map<string, vector<pair<string, pair<int, int>>>> graph;
+            map<string, bool> cityExist;
+            ifstream file("city.txt");
+            string city;
+            while (file >> city) cityExist[toLowerManual(city)] = true;
+            file.close();
+            ifstream pathfile("path.txt");
+            string a, b; int t, c;
+            while (pathfile >> a >> b >> t >> c) {
+            a = toLowerManual(a);
+            b = toLowerManual(b);
+            graph[a].push_back({b, {t, c}});
+            graph[b].push_back({a, {t, c}});
+           }
+           pathfile.close();
+          planMyTrip(graph, cityExist);
+          break;}
 
-    return uniquePaths;
+        case 4:{
+                cout << "\033[1;32mThanks for using Smart Route Finder!\033[0m\n";
+                Sleep(1500);
+                return 0;}
+            default:
+                cout << "\033[1;31mInvalid choice!\033[0m\n";
+                Sleep(1000);
+        }
+        system("cls");
+    }
 }
 
-void findTopKPaths(map<string, vector<Edge>> graph, string start, string end, int K) {
-    vector<FullPath> allPaths;
-    map<string, bool> visited;
-    vector<string> path = {start};
+//Header Function 
+void printHeader() {
+    cout << "\033[1;36m----------------------------------------------\033[0m\n";
+    cout << "\033[1;36m-                                            -\033[0m\n";
+    cout << "\033[1;32m-       SMART ROUTE FINDER SYSTEM            -\033[0m\n";
+    cout << "\033[1;36m-                                            -\033[0m\n";
+    cout << "\033[1;36m----------------------------------------------\033[0m\n";
+}
 
-    dfsAllPaths(start, end, graph, visited, path, 0, allPaths);
-
-    if (allPaths.empty()) {
-        cout << "\033[1;31mNo route found between " << start << " and " << end << "!\033[0m\n";
-        return;
-    }
-
-    sort(allPaths.begin(), allPaths.end(), [](const FullPath& a, const FullPath& b) {
-        return a.totalValue < b.totalValue;
-    });
-    allPaths = removeDuplicatePaths(allPaths);
-    cout << "\n\033[1;34mBest ways to travel from " << start << " to " << end << ":\033[0m\n";  
-    int shown = min(K, (int)allPaths.size());
-    for (int i = 0; i < shown; i++) {
-        cout << "\033[1;36m" << i + 1 << ".\033[0m ";
-        for (int j = 0; j < allPaths[i].route.size(); j++) {
-            cout << allPaths[i].route[j];
-            if (j < allPaths[i].route.size() - 1) cout << " -> ";
+// Function realeted to the path finding process //
+void Alltypeofpath() {
+    int choice;
+    while (true) {
+        cout << "\033[1;35mSelect the type of route you want to find:\033[0m\n";
+        cout << "1. Best cost path\n";
+        cout << "2. Best timing path\n";
+        cout << "3. Best overall path\n";
+        cout << "4. Exit\n";
+        cout << "Enter your choice: ";
+        string input;
+        cin >> input;
+        choice = input[0] - '0';
+        system("cls");
+        switch (choice) {
+            case 1: costpath(); break;
+            case 2: timepath(); break;
+            case 3: overallbest(); break;
+            case 4: return;
+            default: cout << "\033[1;31mInvalid choice!\033[0m\n"; Sleep(1000);
         }
-        cout << "  (\033[1;32m" << allPaths[i].totalValue << "\033[0m)\n";
+        system("cls");
     }
 }
 
@@ -218,36 +276,103 @@ void overallbest() {
     system("pause");
 }
 
-void Alltypeofpath() {
-    int choice;
-    while (true) {
-        cout << "\033[1;35mSelect the type of route you want to find:\033[0m\n";
-        cout << "1. Best cost path\n";
-        cout << "2. Best timing path\n";
-        cout << "3. Best overall path\n";
-        cout << "4. Exit\n";
-        cout << "Enter your choice: ";
-        string input;
-        cin >> input;
-        choice = input[0] - '0';
-        system("cls");
-        switch (choice) {
-            case 1: costpath(); break;
-            case 2: timepath(); break;
-            case 3: overallbest(); break;
-            case 4: return;
-            default: cout << "\033[1;31mInvalid choice!\033[0m\n"; Sleep(1000);
+void findTopKPaths(map<string, vector<Edge>> graph, string start, string end, int K) {
+    vector<FullPath> allPaths;
+    map<string, bool> visited;
+    vector<string> path = {start};
+
+    dfsAllPaths(start, end, graph, visited, path, 0, allPaths);
+
+    if (allPaths.empty()) {
+        cout << "\033[1;31mNo route found between " << start << " and " << end << "!\033[0m\n";
+        return;
+    }
+
+    sort(allPaths.begin(), allPaths.end(), [](const FullPath& a, const FullPath& b) {
+        return a.totalValue < b.totalValue;
+    });
+    allPaths = removeDuplicatePaths(allPaths);
+    cout << "\n\033[1;34mBest ways to travel from " << start << " to " << end << ":\033[0m\n";  
+    int shown = min(K, (int)allPaths.size());
+    for (int i = 0; i < shown; i++) {
+        cout << "\033[1;36m" << i + 1 << ".\033[0m ";
+        for (int j = 0; j < allPaths[i].route.size(); j++) {
+            cout << allPaths[i].route[j];
+            if (j < allPaths[i].route.size() - 1) cout << " -> ";
         }
-        system("cls");
+        cout << "  (\033[1;32m" << allPaths[i].totalValue << "\033[0m)\n";
     }
 }
 
-bool cityExists(const string &city) {
-    ifstream file("city.txt");
-    if (!file.is_open()) return false;
-    string name;
-    while (file >> name) if (name == city) return true;
-    return false;
+vector<FullPath> removeDuplicatePaths(const vector<FullPath>& allPaths) {
+    vector<FullPath> uniquePaths;
+    set<string> seen;
+
+    for (const auto& p : allPaths) {
+        string pathString;
+        for (const auto& city : p.route)
+            pathString += city + "->";
+        pathString.pop_back(); 
+
+        if (!seen.count(pathString)) {
+            seen.insert(pathString);
+            uniquePaths.push_back(p);
+        }
+    }
+
+    return uniquePaths;
+}
+
+void dfsAllPaths(string current, string dest, map<string, vector<Edge>>& graph,
+                 map<string, bool>& visited, vector<string>& path, int cost,
+                 vector<FullPath>& allPaths) {
+    if (current == dest) {
+        allPaths.push_back({path, cost});
+        return;
+    }
+
+    visited[current] = true;
+    for (auto& e : graph[current]) {
+        if (!visited[e.to]) {
+            path.push_back(e.to);
+            dfsAllPaths(e.to, dest, graph, visited, path, cost + e.val, allPaths);
+            path.pop_back(); 
+        }
+    }
+    visited[current] = false;
+}
+//--------------------------------------------------------------------------------------------------------//
+
+// Function realeted to the Path Modifying Process //
+void changeinthepath() {
+    int choice;
+    while (true) {
+        cout << "\033[1;35m--- MODIFY PATH MENU ---\033[0m\n";
+        cout << "1. Add new city\n";
+        cout << "2. Add new path\n";
+        cout << "3. Delete existing path\n";
+        cout << "4. Update existing path\n";
+        cout << "5. Exit\n";
+        cout << "Enter your choice: ";
+        string input;
+        cin >> input;
+        if(input.size()>=2 || input[0]<'0' || input[0]>'9'){
+            cout<<"Invalid Input"<<endl ;
+            Sleep(1000);
+            continue ;
+        }
+        choice = input[0] - '0';
+        system("cls");
+        switch (choice) {
+            case 1: newcity(); break;
+            case 2: newpath(); break;
+            case 3: deletepath(); break;
+            case 4: updatePath(); break;
+            case 5: return;
+            default: cout << "\033[1;31mInvalid Input!\033[0m\n"; Sleep(1000);
+        }
+        system("cls");
+    }
 }
 
 void newcity() {
@@ -337,73 +462,164 @@ void deletepath() {
     Sleep(1500);
 }
 
-void changeinthepath() {
-    int choice;
-    while (true) {
-        cout << "\033[1;35m--- MODIFY PATH MENU ---\033[0m\n";
-        cout << "1. Add new city\n";
-        cout << "2. Add new path\n";
-        cout << "3. Delete existing path\n";
-        cout << "4. Exit\n";
-        cout << "Enter your choice: ";
-        string input;
-        cin >> input;
-        if(input.size()>=2 || input[0]<'0' || input[0]>'9'){
-            cout<<"Invalid Input"<<endl ;
-            Sleep(1000);
-            continue ;
-        }
-        choice = input[0] - '0';
-        system("cls");
-        switch (choice) {
-            case 1: newcity(); break;
-            case 2: newpath(); break;
-            case 3: deletepath(); break;
-            case 4: return;
-            default: cout << "\033[1;31mInvalid Input!\033[0m\n"; Sleep(1000);
-        }
-        system("cls");
-    }
-}
+void updatePath() {
+    showAllCities();
+    string a, b;
+    cout << "\033[1;33mEnter start city:\033[0m ";
+    cin >> a;
+    cout << "\033[1;33mEnter destination city:\033[0m ";
+    cin >> b;
 
-void printHeader() {
-    cout << "\033[1;36m----------------------------------------------\033[0m\n";
-    cout << "\033[1;36m-                                            -\033[0m\n";
-    cout << "\033[1;32m-       SMART ROUTE FINDER SYSTEM            -\033[0m\n";
-    cout << "\033[1;36m-                                            -\033[0m\n";
-    cout << "\033[1;36m----------------------------------------------\033[0m\n";
-}
+    a = toLowerManual(a);
+    b = toLowerManual(b);
 
-int main() {
-    printHeader();
-    Sleep(2000);
-    system("cls");
-    while (true) {
-        cout << "\033[1;35m--- MAIN MENU ---\033[0m\n";
-        cout << "1. Find a path\n";
-        cout << "2. Modify paths/cities\n";
-        cout << "3. Exit\n";
-        cout << "Enter your choice: ";
-        string input;
-        cin >> input;
-        if(input.size()>=2 || (input[0]<'0' && input[0]>'9')){
-            cout<<"Invalid Input"<<endl ;
-            Sleep(1000);
-            continue ;
+    ifstream infile("path.txt");
+    vector<string> lines;
+    string line;
+    bool updated = false;
+
+    while (getline(infile, line)) {
+        if (line.empty()) continue;
+        string c1, c2;
+        int t, c;
+        stringstream ss(line);
+        ss >> c1 >> c2 >> t >> c;
+
+        if ((c1 == a && c2 == b) || (c1 == b && c2 == a)) {
+            cout << "\033[1;33mEnter new travel time:\033[0m ";
+            cin >> t;
+            cout << "\033[1;33mEnter new travel cost:\033[0m ";
+            cin >> c;
+            lines.push_back(a + " " + b + " " + to_string(t) + " " + to_string(c));
+            updated = true;
+        } else {
+            lines.push_back(line);
         }
-        int choice = input[0] - '0';
-        system("cls");
-        switch (choice) {
-            case 1: Alltypeofpath(); break;
-            case 2: changeinthepath(); break;
-            case 3:
-                cout << "\033[1;32mThanks for using Smart Route Finder!\033[0m\n";
-                Sleep(1500);
-                return 0;
-            default:
-                cout << "\033[1;31mInvalid choice!\033[0m\n";
-                Sleep(1000);
-        }
-        system("cls");
     }
+    infile.close();
+
+    ofstream outfile("path.txt");
+    for (auto &l : lines)
+        outfile << l << "\n";
+    outfile.close();
+
+    if (updated)
+        cout << "\033[1;32mPath updated successfully!\033[0m\n";
+    else
+        cout << "\033[1;31mPath not found!\033[0m\n";
+    Sleep(1500);
+}
+//--------------------------------------------------------------------------------------------------------//
+// Function for plannig the trip 
+void planMyTrip(map<string, vector<pair<string, pair<int, int>>>> &graph, map<string, bool> &cityExist) {
+    showAllCities();
+    string start;
+    cout << "\033[1;36mEnter your starting city: \033[0m";
+    cin >> start;
+    start = toLowerManual(start);
+
+    if (!cityExist[start]) {
+        cout << "\033[1;31mStarting city not found!\033[0m\n";
+        Sleep(1500);
+        return;
+    }
+
+    int n;
+    cout << "\033[1;36mEnter how many cities you want to visit (2-3 recommended): \033[0m";
+    cin >> n;
+
+    vector<string> tripCities(n);
+    cout << "\033[1;36mEnter the cities you want to visit:\033[0m\n";
+    for (int i = 0; i < n; i++) {
+        cout << "City " << i + 1 << ": ";
+        cin >> tripCities[i];
+        tripCities[i] = toLowerManual(tripCities[i]);
+        if (!cityExist[tripCities[i]]) {
+            cout << "\033[1;31mCity not found: " << tripCities[i] << "\033[0m\n";
+            Sleep(1500);
+            return;
+        }
+    }
+
+    vector<int> idx(n);
+    iota(idx.begin(), idx.end(), 0);
+    int bestCost = INT_MAX;
+    int bestTime = INT_MAX;
+    vector<string> bestRoute;
+
+    do {
+        int totalCost = 0;
+        int totalTime = 0;
+        string current = start;
+        bool valid = true;
+
+        for (int i : idx) {
+            string next = tripCities[i];
+
+            // Dijkstra (Cost-based)
+            map<string, int> distCost, distTime;
+            for (auto &p : graph) {
+                distCost[p.first] = INT_MAX;
+                distTime[p.first] = INT_MAX;
+            }
+            distCost[current] = 0;
+            distTime[current] = 0;
+
+            priority_queue<pair<int, string>, vector<pair<int, string>>, greater<pair<int, string>>> pq;
+            pq.push({0, current});
+
+            while (!pq.empty()) {
+                pair<int, string> top = pq.top();
+                pq.pop();
+                int cost = top.first;
+                string city = top.second;
+                if (cost > distCost[city]) continue;
+                for (auto &edge : graph[city]) {
+                string nei = edge.first;
+                int t = edge.second.first; 
+                int c = edge.second.second;
+                if (distCost[city] + c < distCost[nei]) {
+                  distCost[nei] = distCost[city] + c;
+                  distTime[nei] = distTime[city] + t;
+                  pq.push({distCost[nei], nei});
+                }
+            }
+
+        }
+
+            if (distCost[next] == INT_MAX) {
+                valid = false;
+                break;
+            }
+
+            totalCost += distCost[next];
+            totalTime += distTime[next];
+            current = next;
+        }
+
+        if (valid && totalCost < bestCost) {
+            bestCost = totalCost;
+            bestTime = totalTime;
+            bestRoute.clear();
+            bestRoute.push_back(start);
+            for (int i : idx)
+                bestRoute.push_back(tripCities[i]);
+        }
+
+    } while (next_permutation(idx.begin(), idx.end()));
+
+    if (bestRoute.empty()) {
+        cout << "\033[1;31mNo valid route found for all cities!\033[0m\n";
+        Sleep(2000);
+        return;
+    }
+
+    cout << "\n\033[1;32m--- Cheapest Trip Plan ---\033[0m\n";
+    for (int i = 0; i < bestRoute.size(); i++) {
+        cout << bestRoute[i];
+        if (i != bestRoute.size() - 1) cout << " -> ";
+    }
+    cout << "\n\033[1;33mTotal Estimated Cost: " << bestCost << "\033[0m\n";
+    cout << "\033[1;34mTotal Estimated Time: " << bestTime << " hours\033[0m\n";
+    Sleep(4000);
 }
